5e40b48c35e0dd6d77b0a00f751e7b9203f3d63e
Brian_Izaki <bkizaki@outlook.com>
2022-04-06 01:50:18 -0300

    feat init card

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
+body {
   font-family: $family;
+  background-color: $gray_100;
+  color: $dark;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $dark;
 }
 </style>
